04da505a1031f77d95acd42b1b00b03663392b55
No101011 <2294646820@qq.com>
2026-03-15 16:20:30 +0800

    是我眼睛瞎了，没看见flush

--- rtl/core/pc_reg.v
+++ rtl/core/pc_reg.v
@@ -2,9 +2,8 @@ module Pc_reg (
     input clk,
     input rst_n,
     input stall,
-    input flush,
     //跳转
-    input pc_branch_en,  //端口规范中少了跳转使能
+    input flush,
     input [31:0] pc_branch_target,
     //输出指令地址
     output reg [31:0] pc_out
@@ -12,7 +11,7 @@ module Pc_reg (
   always @(posedge clk) begin
     if (rst_n) begin
       pc_out <= 32'd0;
-    end else if (pc_branch_en) begin
+    end else if (flush) begin
       pc_out <= pc_branch_target;
     end else if (~stall) begin
       pc_out <= pc_out + 32'd4;
