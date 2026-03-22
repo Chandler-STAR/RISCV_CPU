585fb76504cc4630cfeb6d23f659ab0d3c7e2c97
Chandler-STAR <3311500641@qq.com>
2026-03-22 17:19:22 +0800

    更正输出信号类型，以及纠正x0逻辑

--- rtl/core/regfile.v
+++ rtl/core/regfile.v
@@ -1,17 +1,16 @@
 `include "defines.vh"
 
-module regfile(
-    input clk,
-    input [4:0] rs1_addr,
-    input [4:0] rs2_addr,
-    input [4:0] rd_addr,
-    input [31:0] wd,
-    input reg_we,
-    output reg [31:0] rs1_rf,
-    output reg [31:0] rs2_rf
+module regfile (
+    input  wire        clk,
+    input  wire [4:0]  rs1_addr, 
+    input  wire [4:0]  rs2_addr, 
+    output wire [31:0] rs1,
+    output wire [31:0] rs2,
+    input  wire [4:0]  rd_addr,  
+    input  wire [31:0] wd,       
+    input  wire        reg_we    
 );
-
-    x0 = 0; // x0寄存器始终为0
+    reg [31:0] rf [0:31]; // 只需要定义 1-31，x0 逻辑由硬件保证
     //异步读
     assign rs1 = (rs1_addr == 5'd0) ? 32'd0 : rf[rs1_addr];// 读寄存器1，x0寄存器始终为0
     assign rs2 = (rs2_addr == 5'd0) ? 32'd0 : rf[rs2_addr];// 读寄存器2，x0寄存器始终为0
