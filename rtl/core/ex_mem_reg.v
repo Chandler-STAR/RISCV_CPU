58437587c5b1ad58b283c5fddadfece51a6acacd
No101011 <2294646820@qq.com>
2026-03-23 15:18:57 +0800

    update readme ,coding ex_mem_reg.v

---
+++ rtl/core/ex_mem_reg.v
@@ -0,0 +1,14 @@
+module ex_mem_reg (
+    input  clk,
+    input  rst,
+    input  [31:0]pc4_in,
+    input  [31:0]instr_in,
+    input  [31:0]alu_out_in,
+    input  [31:0]rs2_in,
+    input  [4:0]rd_addr_in,
+    input  reg_we_in,
+    input  mem_we_in,
+
+);
+
+endmodule //ex_mem_reg
