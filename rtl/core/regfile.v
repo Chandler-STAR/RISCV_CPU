2bc37577ed62e8325ccf9d10572273cef9647afe
zyx316006 <2726884622@qq.com>
2026-03-30 00:06:28 +0800

    解决仿真出现的问题

--- rtl/core/regfile.v
+++ rtl/core/regfile.v
@@ -1,27 +1,34 @@
 `include "../include/defines.vh"
 
 module regfile (
-    input         clk,
-    input  [ 4:0] rs1_addr,
-    input  [ 4:0] rs2_addr,
-    output [31:0] rs1_rf,
-    output [31:0] rs2_rf,
-    input  [ 4:0] rd_addr,
-    input  [31:0] wd,
-    input         reg_we
+    input          clk,
+    input  [ 4:0]  rs1_addr,
+    input  [ 4:0]  rs2_addr,
+    output [31:0]  rs1_rf,
+    output [31:0]  rs2_rf,
+    input  [ 4:0]  rd_addr,
+    input  [31:0]  wd,
+    input          reg_we
 );
-  reg [31:0] rf[0:31];  // 只需要定义 1-31，x0 逻辑由硬件保证
-  //异步读
-  initial begin
-    rf[0] = 32'h0;  // 初始化寄存器文件
-  end
+    reg [31:0] rf[0:31];
+    integer i;
 
-  assign rs1_rf = (rs1_addr == 5'b0) ? 32'b0 : rf[rs1_addr];  // 读寄存器1，x0寄存器始终为0
-  assign rs2_rf = (rs2_addr == 5'b0) ? 32'b0 : rf[rs2_addr];  // 读寄存器2，x0寄存器始终为0
+    initial begin
+        for (i = 0; i < 32; i = i + 1) begin
+            rf[i] = 32'h0;
+        end
+    end
+
+    assign rs1_rf = (rs1_addr == 5'b0) ? 32'b0 : rf[rs1_addr];  // 读寄存器1，x0寄存器始终为0
+    assign rs2_rf = (rs2_addr == 5'b0) ? 32'b0 : rf[rs2_addr];  // 读寄存器2，x0寄存器始终为0
 
 
-  //同步写    x0寄存器始终为0，写入x0寄存器的操作被忽略
-  always @(posedge clk) begin
-    if (reg_we && rd_addr != 5'b0) rf[rd_addr] <= wd;
-  end
+    // 同步写逻辑（保持不变）
+    always @(negedge clk) begin   //上升沿改成下降沿，使得readfile提前进行，解决仿真问题
+        if (reg_we && rd_addr != 5'b0) begin
+            rf[rd_addr] <= wd;
+        end
+    end
+
+
 endmodule
