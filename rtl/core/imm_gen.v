5f68ef094f59e0dc08bdb43c07e73aabe542a62f
Chandler-STAR <3311500641@qq.com>
2026-05-17 12:26:01 +0800

    Zicsr立即数生成

--- rtl/core/imm_gen.v
+++ rtl/core/imm_gen.v
@@ -7,14 +7,21 @@ module imm_gen (
     imm = 32'b0;  // [FIX Bug9] 默认赋值，防止综合推断 latch
     case (d_instr[6:0])
       7'b0010011, 7'b0000011, 7'b1100111: imm = {{20{d_instr[31]}}, d_instr[31:20]};  // I-type
+
       7'b0100011: imm = {{20{d_instr[31]}}, d_instr[31:25], d_instr[11:7]};  // S-type
+
       7'b1100011:
       imm = {
         {20{d_instr[31]}}, d_instr[31], d_instr[7], d_instr[30:25], d_instr[11:8], 1'b0
       };  // B-type
+
       7'b0110111, 7'b0010111: imm = {d_instr[31:12], 12'b0};  // U-type
+
       7'b1101111:
       imm = {{12{d_instr[31]}}, d_instr[19:12], d_instr[20], d_instr[30:21], 1'b0};  // J-type
+
+      7'b1110011: imm = {27'b0, d_instr[19:15]};  // Zicsr uimm
+
       default: imm = 32'b0;
     endcase
   end
